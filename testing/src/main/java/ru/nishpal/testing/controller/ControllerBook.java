2979a81bfc9f77d1cf3047398100878cd1a9239b
daniilnishpal <nispaldaniil512@gamil.com>
2023-04-22 10:37:13 +0300

    add repository

--- testing/src/main/java/ru/nishpal/testing/controller/ControllerBook.java
+++ testing/src/main/java/ru/nishpal/testing/controller/ControllerBook.java
@@ -1,47 +1,40 @@
 package ru.nishpal.testing.controller;
 
 import org.springframework.web.bind.annotation.*;
-import ru.nishpal.testing.model.Book;
 import ru.nishpal.testing.model.dto.BookDto;
+import ru.nishpal.testing.repository.BookRepository;
 import ru.nishpal.testing.service.BookService;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/v1/book")
 public class ControllerBook {
-    private List<Book> books = Stream.of(
-            new Book(1L, "Master and Margarita", "Bulgakov","Roman"),
-            new Book(2L, "War and Peace", "Tolstoy","Military prose"),
-            new Book(3L, "Crime and Punishment", "Dostoyevsky", "Roman"),
-            new Book(4L, "Fathers and childrens", "Turgenev","Roman")
-    ).collect(Collectors.toList());
-
+    private BookRepository bookRepository;
     private final BookService bookService;
 
-    public ControllerBook(BookService bookService) {
+    public ControllerBook(BookRepository bookRepository, BookService bookService) {
+        this.bookRepository = bookRepository;
         this.bookService = bookService;
     }
 
     @GetMapping
     public List<BookDto> getBooks() {
-        return BookDto.bookListToDto(books);
+        return BookDto.bookListToDto(bookRepository.getBooks());
     }
 
     @GetMapping("/{genre}")
     public List<BookDto> getBookByGenre(@PathVariable String genre) {
-        return BookDto.bookListToDto(bookService.findBooksByGenre(this.books, genre));
+        return BookDto.bookListToDto(bookService.findBooksByGenre(bookRepository.getBooks(), genre));
     }
 
     @PostMapping
     public void postBook(@RequestBody BookDto bookDto) {
-        bookService.createBook(bookDto, this.books);
+        bookService.createBook(bookDto, bookRepository.getBooks());
     }
 
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable long id) {
-        bookService.deleteBookById(id, this.books);
+        bookService.deleteBookById(id, bookRepository.getBooks());
     }
 }
---
+++ testing/src/main/java/ru/nishpal/testing/repository/BookRepository.java
@@ -0,0 +1,18 @@
+package ru.nishpal.testing.repository;
+
+import lombok.Data;
+import org.springframework.stereotype.Repository;
+import ru.nishpal.testing.model.Book;
+
+import java.util.List;
+
+@Repository
+@Data
+public class BookRepository {
+    private List<Book> books = List.of(
+            new Book(1L, "Master and Margarita", "Bulgakov","Roman"),
+            new Book(2L, "War and Peace", "Tolstoy","Military prose"),
+            new Book(3L, "Crime and Punishment", "Dostoyevsky", "Roman"),
+            new Book(4L, "Fathers and childrens", "Turgenev","Roman")
+    );
+}
